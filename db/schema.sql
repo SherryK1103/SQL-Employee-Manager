03c3d0ded1d35e186a402f8fd6b2fca977e841f8
SherryK1103 <129017117+SherryK1103@users.noreply.github.com>
2023-08-26 13:45:27 -0500

    edited schema sql file to have relevant tables and categories

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,28 @@
-DROP DATABASE IF EXISTS employee_db;
+CREATE TABLE
+    departments (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(255) NOT NULL
+    );
 
-CREATE DATABASE employee_db;
+CREATE TABLE
+    roles (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        title VARCHAR(255) NOT NULL,
+        salary DECIMAL(10, 2) NOT NULL,
+        department_id INT,
+        FOREIGN KEY (department_id) REFERENCES departments(id)
+    );
 
-USE employee_db;
-
-CREATE TABLE (
-        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        first_name VARCHAR(30) NOT NULL,
-        last_name VARCHAR(30) NOT NULL,
-        enrolled BOOLEAN NOT NULL
+CREATE TABLE
+    employees (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        first_name VARCHAR(255) NOT NULL,
+        last_name VARCHAR(255) NOT NULL,
+        role_id INT,
+        department_id INT,
+        salary DECIMAL(10, 2),
+        manager_id INT,
+        FOREIGN KEY (role_id) REFERENCES roles(id),
+        FOREIGN KEY (department_id) REFERENCES departments(id),
+        FOREIGN KEY (manager_id) REFERENCES employees(id)
     );

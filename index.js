f6bfc7ada49b4863b230d8b1a9e0f3663b188c82
SherryK1103 <129017117+SherryK1103@users.noreply.github.com>
2023-08-26 14:18:14 -0500

    updated objects arrays in index js file

--- index.js
+++ index.js
@@ -38,106 +38,39 @@ const questions = [
 // Choose add an employee, prompted to enter employee's first name, last name, role, and manager
 
 // Choose update an employee role, thenprompted to select an employee to update their new role and this info is updated in the DB
-
-
- // do a switch statement based on whatever choice
-
- const viewAllDepartments = [ 
-    {
-        name: "text",
-        type: "input",
-        message: "Enter three characters",
-    },
-
-    {
-        name: "text_color",
-        type: "input",
-        message: "Insert a color for text",
-    },
-
-    {
-        name: "shapes",
-        type: "list",
-        message: "Choose a shape for your logo",
-        choices: [
-            "Circle",
-            "Square",
-            "Triangle",
-        ]
-    },
-
-    {
-        name: "shapes_color",
-        type: "input",
-        message: "Enter a shape color",
-    },
-];
-
-
-const viewAllRoles = [ 
-    {
-        name: "text",
-        type: "input",
-        message: "Enter three characters",
-    },
-
-    {
-        name: "text_color",
-        type: "input",
-        message: "Insert a color for text",
-    },
-
-    {
-        name: "shapes",
-        type: "list",
-        message: "Choose a shape for your logo",
-        choices: [
-            "Circle",
-            "Square",
-            "Triangle",
-        ]
-    },
-
-    {
-        name: "shapes_color",
-        type: "input",
-        message: "Enter a shape color",
-    },
-];
-
-
-const viewAllEmployees = [ 
-    {
-        name: "text",
-        type: "input",
-        message: "Enter three characters",
-    },
-
-    {
-        name: "text_color",
-        type: "input",
-        message: "Insert a color for text",
-    },
-
-    {
-        name: "shapes",
-        type: "list",
-        message: "Choose a shape for your logo",
-        choices: [
-            "Circle",
-            "Square",
-            "Triangle",
-        ]
-    },
-
-    {
-        name: "shapes_color",
-        type: "input",
-        message: "Enter a shape color",
-    },
-];
-
+      
 const addDepartment = [ 
+    {
+        name: "text",
+        type: "input",
+        message: "Enter three characters",
+    },
+
+    {
+        name: "text_color",
+        type: "input",
+        message: "Insert a color for text",
+    },
+
+    {
+        name: "shapes",
+        type: "list",
+        message: "Choose a shape for your logo",
+        choices: [
+            "Circle",
+            "Square",
+            "Triangle",
+        ]
+    },
+
+    {
+        name: "shapes_color",
+        type: "input",
+        message: "Enter a shape color",
+    },
+];
+
+const addRole = [ 
     {
         name: "text",
         type: "input",

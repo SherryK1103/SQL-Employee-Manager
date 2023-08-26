e51a3d5f4e9175045cf7614d9fc4a4d06435599f
SherryK1103 <129017117+SherryK1103@users.noreply.github.com>
2023-08-26 12:17:41 -0500

    added templates for consts for the prompts

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
-const { Circle, Triangle, Square } = require('./lib/shapes');
+
 
 // TODO: Create an array of questions for user input
 const questions = [ 
@@ -22,9 +22,210 @@ const questions = [
     },
 ];
 
+// Choose all departments:
+// job title, role ID, department, salary
+
+// Choose View All Employees
+// formatted table showing employee data including emp ID, first name, last name, job title, departments, salaries, and managers
+
+
+// Choose Add a Department, then prompted tp enter a name, salary, and department for the role and it's added to DB
+
+// Choose add an employee, prompted to enter employee's first name, last name, role, and manager
+
+// Choose update an employee role, thenprompted to select an employee to update their new role and this info is updated in the DB
+
+
  // do a switch statement based on whatever choice
 
- 
+ const viewAllDepartments = [ 
+    {
+        name: "text",
+        type: "input",
+        message: "Enter three characters",
+    },
+
+    {
+        name: "text_color",
+        type: "input",
+        message: "Insert a color for text",
+    },
+
+    {
+        name: "shapes",
+        type: "list",
+        message: "Choose a shape for your logo",
+        choices: [
+            "Circle",
+            "Square",
+            "Triangle",
+        ]
+    },
+
+    {
+        name: "shapes_color",
+        type: "input",
+        message: "Enter a shape color",
+    },
+];
+
+
+const viewAllRoles = [ 
+    {
+        name: "text",
+        type: "input",
+        message: "Enter three characters",
+    },
+
+    {
+        name: "text_color",
+        type: "input",
+        message: "Insert a color for text",
+    },
+
+    {
+        name: "shapes",
+        type: "list",
+        message: "Choose a shape for your logo",
+        choices: [
+            "Circle",
+            "Square",
+            "Triangle",
+        ]
+    },
+
+    {
+        name: "shapes_color",
+        type: "input",
+        message: "Enter a shape color",
+    },
+];
+
+
+const viewAllEmployees = [ 
+    {
+        name: "text",
+        type: "input",
+        message: "Enter three characters",
+    },
+
+    {
+        name: "text_color",
+        type: "input",
+        message: "Insert a color for text",
+    },
+
+    {
+        name: "shapes",
+        type: "list",
+        message: "Choose a shape for your logo",
+        choices: [
+            "Circle",
+            "Square",
+            "Triangle",
+        ]
+    },
+
+    {
+        name: "shapes_color",
+        type: "input",
+        message: "Enter a shape color",
+    },
+];
+
+const addDepartment = [ 
+    {
+        name: "text",
+        type: "input",
+        message: "Enter three characters",
+    },
+
+    {
+        name: "text_color",
+        type: "input",
+        message: "Insert a color for text",
+    },
+
+    {
+        name: "shapes",
+        type: "list",
+        message: "Choose a shape for your logo",
+        choices: [
+            "Circle",
+            "Square",
+            "Triangle",
+        ]
+    },
+
+    {
+        name: "shapes_color",
+        type: "input",
+        message: "Enter a shape color",
+    },
+];
+
+const addEmployee = [ 
+    {
+        name: "text",
+        type: "input",
+        message: "Enter three characters",
+    },
+
+    {
+        name: "text_color",
+        type: "input",
+        message: "Insert a color for text",
+    },
+
+    {
+        name: "shapes",
+        type: "list",
+        message: "Choose a shape for your logo",
+        choices: [
+            "Circle",
+            "Square",
+            "Triangle",
+        ]
+    },
+
+    {
+        name: "shapes_color",
+        type: "input",
+        message: "Enter a shape color",
+    },
+];
+
+const updateEmployeeRole = [ 
+    {
+        name: "text",
+        type: "input",
+        message: "Enter three characters",
+    },
+
+    {
+        name: "text_color",
+        type: "input",
+        message: "Insert a color for text",
+    },
+
+    {
+        name: "shapes",
+        type: "list",
+        message: "Choose a shape for your logo",
+        choices: [
+            "Circle",
+            "Square",
+            "Triangle",
+        ]
+    },
+
+    {
+        name: "shapes_color",
+        type: "input",
+        message: "Enter a shape color",
+    },
+];
+
 
 // function generateSVG(answers, shape) {
 //     let svg = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">`;
